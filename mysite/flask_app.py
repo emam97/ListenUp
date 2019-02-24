6aab6058aec4ddd80cb2b8ae94913791d4a32ef8
Emily Marin <emily.m.a.marin@gmail.com>
2019-02-24 15:36:42 -0500

    Attempt to connect to MySQL database

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -8,19 +8,41 @@ from twilio.twiml.messaging_response import Message, MessagingResponse
 import os
 from twilio.http.http_client import TwilioHttpClient
 from twilio.twiml.voice_response import Gather, VoiceResponse
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="emarin6",
+    password="listenup1",
+    hostname="emarin6.mysql.pythonanywhere-services.com",
+    databasename="emarin6$ListenUp",
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 proxy_client = TwilioHttpClient()
 proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
 
 account_sid = 'AC9d38ae9e70ccaee447b341b68b3f1333'
 auth_token = '08910c27a7a5163bcc0ed69421bf7c94'
 
 client = Client(account_sid, auth_token, http_client=proxy_client)
+
+class Transcription(db.Model):
+
+    __tablename__ = "transcriptions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(4096))
+    transcription = db.Column(db.String(4096))
 
 @app.route("/", methods=["GET", "POST"])
 def record_call():
@@ -45,4 +67,14 @@ def transcribing():
     print 'RECORDING URL: ' + str(recording_url)
     print 'Transcription Id: ' + str(request.values.get('TranscriptionSid'))
     print 'Transcription: ' + str(request.values.get('TranscriptionText'))
+
+    addTranscriptionToDatabase(str(recording_url), str(request.values.get('TranscriptionText')))
     return str(request.values.get('TranscriptionText'))
+
+
+def addTranscriptionToDatabase(url, transcription):
+    tr = Transcription()
+    tr.url = url
+    tr.transcription = transcription
+    db.session.add(tr)
+    db.session.commit()
