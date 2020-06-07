4f17ebbc1494c9bd554cdffe17c8cd90c29b6c28
Avanti <avantij14@gmail.com>
2020-06-07 19:49:57 -0400

    remove auth tokens

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -8,53 +8,32 @@ from twilio.twiml.messaging_response import Message, MessagingResponse
 import os
 from twilio.http.http_client import TwilioHttpClient
 from twilio.twiml.voice_response import Gather, VoiceResponse
-from flask_sqlalchemy import SQLAlchemy
 import requests
 import speech_recognition as sr
-
+from ibm_watson import ToneAnalyzerV3
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 import webbrowser
+import json
+import pymongo
+from pymongo import MongoClient
+import configparser
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="avantij14",
-    password="password1",
-    hostname="avantij14.mysql.pythonanywhere-services.com",
-    databasename="avantij14$ListenUp",
-)
+config = configparser.ConfigParser()
+config.read('/home/ajoshi/listen_up_config')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+account_sid = config['TWILIO']['twilio_sid']
+auth_token = config['TWILIO']['twilio_auth_token']
 
-db = SQLAlchemy(app)
+client = Client(account_sid, auth_token)
 
-proxy_client = TwilioHttpClient()
-proxy_client.session.proxies = {'https': os.environ['https_proxy']}
-
-
-account_sid = 'AC9d38ae9e70ccaee447b341b68b3f1333'
-auth_token = '08910c27a7a5163bcc0ed69421bf7c94'
-
-client = Client(account_sid, auth_token, http_client=proxy_client)
-
-class Transcription(db.Model):
-
-    __tablename__ = "transcriptions"
-
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(4096))
-    transcription = db.Column(db.String(4096))
-
-class Sentiment(db.Model):
-
-    __tablename__ = "sentiments"
-
-    id = db.Column(db.Integer, primary_key=True)
-    noun = db.Column(db.String(4096))
-    pos_count = db.Column(db.String(4096))
-    neg_count = db.Column(db.String(4096))
+mongo_client = MongoClient(config['MONGO']['client'], int(config['MONGO']['port']))
+db = mongo_client.listen_up
+collection = db.voicemail_analysis
 
 @app.route("/", methods=["GET", "POST"])
 def record_call():
@@ -62,7 +41,7 @@ def record_call():
     if request.method == "POST":
         response = "Call in progess"
         response = VoiceResponse()
-        response.say("Start taking notes after the beep, press # when finished.")
+        response.say("Start talking, press # when finished.")
         response.record(transcribe=True, transcriptionType='slow' ,play_beep='true', max_length=20, finish_on_key='#', transcribeCallback="/transcribing")
 
         return str(response)
@@ -76,30 +55,57 @@ def transcribing():
 
     recording_url = request.values.get('RecordingUrl')
 
-    #print "RECORDING URL: " + str(recording_url)
-    #print "Transcription Id: " + str(request.values.get('TranscriptionSid'))
-    #print "Transcription: " + str(request.values.get('TranscriptionText'))
-
-
-   
     t = requests.get(recording_url, allow_redirects=True)
+    open('recording', 'wb').write(t.content)
+
     # instantiating speech recognition object
     r=sr.Recognizer()
     # creating recording from url
-    recording = sr.AudioFile(t)
+    recording = sr.AudioFile('recording')
     with recording as source:
         audio = r.record(source)
     #printing audio transcription
     print(r.recognize_google(audio))
-
+    analyzeTextSentiment(str(r.recognize_google(audio)))
+    analyzeTextKeywords(str(r.recognize_google(audio)))
     #next step - add transcription to database
-    addTranscriptionToDatabase(str(recording_url), str(r.recognize_google(audio)))
     return str(request.values.get('TranscriptionText'))
 
 
-def addTranscriptionToDatabase(url, transcription):
-    tr = Transcription()
-    tr.url = url
-    tr.transcription = transcription
-    db.session.add(tr)
-    db.session.commit()
+def analyzeTextSentiment(text):
+    authenticator = IAMAuthenticator(config['WATSON']['text_analysis_auth'])
+    tone_analyzer = ToneAnalyzerV3(
+        version='2019-07-12',
+        authenticator=authenticator
+    )
+
+    tone_analyzer.set_service_url(config['WATSON']['text_analysis_service_url'])
+ 
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        content_type='application/json'
+    ).get_result()
+    print(json.dumps(tone_analysis, indent=2))
+    x = collection.insert_one(tone_analysis)
+
+def analyzeTextKeywords(text):
+    authenticator = IAMAuthenticator(config['WATSON']['keyword_auth'])
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2019-07-12',
+        authenticator=authenticator
+    )
+    natural_language_understanding.set_service_url(config['WATSON']['keyword_service_url'])
+
+    response = natural_language_understanding.analyze(
+    text=text,
+    features=Features(
+        entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+        keywords=KeywordsOptions(emotion=True, sentiment=True,
+                                 limit=2))).get_result()
+
+    print(json.dumps(response, indent=2))
+    x = collection.insert_one(response)
+
+
+if __name__=="__main__":
+    app.run()
